cdffa56eebac4e687f0fc769e643bab054458eca
Julio Ureña <juliourena33@gmail.com>
2020-06-05 18:16:56 -0400

    Update smbgetall.sh

--- hackthebox/smbgetall.sh
+++ hackthebox/smbgetall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script usado para nest, permite descargar todos los archivos de todos los directorios compartidos
 # Requiere smbmap y smbclient
-# https://github.com/juliourena/plaintext/hackthebox/smbgetall.sh
+# https://github.com/juliourena/plaintext/blob/master/hackthebox/smbgetall.sh
 
 username=$1
 password=$2
