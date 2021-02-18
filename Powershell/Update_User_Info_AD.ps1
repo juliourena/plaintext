86d13ab3741172c9a27f411c73a67c510ea94065
Julio Ureña <juliourena33@gmail.com>
2021-02-18 15:17:55 -0400

    Update Update_User_Info_AD.ps1
    Change email by SamAccountName

--- Powershell/Update_User_Info_AD.ps1
+++ Powershell/Update_User_Info_AD.ps1
@@ -4,41 +4,41 @@ $users=import-csv c:\users.csv
 foreach($user in $users)
 {
     # Clear variables
-    $email = $null
-    $managerEmail = $null
+    $sam = $null
+    $managerSAM = $null
     $userselected = $null
     $manager = $null
 
-    $email = $user.EmailAddress
-    # Write-Host "User: "$email 
+    $sam = $user.SamAccountName
+    # Write-Host "User: "$sam 
 
-    $managerEmail = $user.Manager
-    # Write-Host "Manager: " $managerEmail
+    $managerSAM = $user.Manager
+    # Write-Host "Manager: " $managerSAM
 
 	try {
-		$userselected = Get-ADUser -Filter {EmailAddress -eq $email}
-        # Write-Host "userselected: " $userselected
+		$userselected = Get-ADUser -Filter {SamAccountName -eq $sam}
+        	# Write-Host "userselected: " $userselected
 
 		$userselected.EmployeeID = $user.EmployeeID 
-        $userselected.EmployeeNumber = $user.EmployeeNumber
-        $userselected.GivenName = $user.GivenName
-        $userselected.Surname = $user.Surname
-        $userselected.Description = $user.Description
-        $userselected.Title = $user.Title
-        $userselected.Department = $user.Department
-        $userselected.EmailAddress = $user.EmailAddress
-        $userselected.OfficePhone = $user.OfficePhone
-        $userselected.MobilePhone = $user.MobilePhone
-        $userselected.Office = $user.Office
-        $userselected.Company = $user.Company
+		$userselected.EmployeeNumber = $user.EmployeeNumber
+		$userselected.GivenName = $user.GivenName
+		$userselected.Surname = $user.Surname
+		$userselected.Description = $user.Description
+		$userselected.Title = $user.Title
+		$userselected.Department = $user.Department
+		$userselected.EmailAddress = $user.EmailAddress
+		$userselected.OfficePhone = $user.OfficePhone
+		$userselected.MobilePhone = $user.MobilePhone
+		$userselected.Office = $user.Office
+		$userselected.Company = $user.Company
         
         # Update User information
         Set-ADUser -Instance $userselected    
 
         # Check If there is any manager define
-        if ($managerEmail)
+        if ($managerSAM)
         { 
-            $manager = Get-ADUser -Filter {EmailAddress -eq $managerEmail}
+            $manager = Get-ADUser -Filter {SamAccountName -eq $managerSAM}
             # Write-Host "Manager Identity: " $manager
 
             $managerSAN = $manager.SamAccountName
@@ -49,6 +49,6 @@ foreach($user in $users)
 	}
 	catch {
 		Write-Host "[-] User not found: " $user.GivenName $user.Surname
-        Write-Host $_
+        	Write-Host $_
 	}
 }
