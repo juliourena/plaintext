bc1e8e8fcb45c643229baa17c90f5ea497fbfc06
Julio Ureña <juliourena33@gmail.com>
2022-09-23 13:50:48 -0400

    Update cookieextractor.py
    Update SQLite queries

--- Scripts/cookieextractor.py
+++ Scripts/cookieextractor.py
@@ -9,12 +9,14 @@ def main(dbpath, host, cookie):
 	conn = sqlite3.connect(dbpath)
 	cursor = conn.cursor()
 
-	if (host == ""):
+	if (host == "" and cookie == ""):
 		query = "SELECT * FROM moz_cookies"
-	elif (cookie != ""):
-		query = "SELECT * FROM moz_cookies WHERE host LIKE '%{}%' AND name = '{}'".format(host, cookie)
-	else:
+	elif (host != "" and cookie == ""):
 		query = "SELECT * FROM moz_cookies WHERE host LIKE '%{}%'".format(host)
+	elif (host == "" and cookie != ""):
+		query = "SELECT * FROM moz_cookies WHERE name LIKE '%{}%'".format(cookie)
+	elif (host != "" and cookie != ""):
+		query = "SELECT * FROM moz_cookies WHERE name LIKE '%{}%' AND host LIKE '%{}%'".format(cookie, host)
 
 	cursor.execute(query)
 	records = cursor.fetchall()
@@ -30,8 +32,8 @@ def main(dbpath, host, cookie):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--dbpath", "-d", required=True, help="the path to the sqlite cookies database")
-	parser.add_argument("--host", "-o", required=False, help="the host for the cookie", default="")
-	parser.add_argument("--cookie", "-c", required=False, help="the name of the cookie", default="")
+	parser.add_argument("--dbpath", "-d", required=True, help="The path to the sqlite cookies database")
+	parser.add_argument("--host", "-o", required=False, help="The host for the cookie", default="")
+	parser.add_argument("--cookie", "-c", required=False, help="The name of the cookie", default="")
 	args = parser.parse_args()
 	main(args.dbpath, args.host, args.cookie)
